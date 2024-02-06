2e9d6ef5a3079c94c966328143bbc7e28315c98f
MasaGt <gachapin2236tx@gmail.com>
2024-02-06 16:27:56 +1300

    Tweak Card style - cahnge paragraph to h2 so it has more appropriate semantic meaning. - add alt to images.

--- aribnb_clone/components/Card.jsx
+++ aribnb_clone/components/Card.jsx
@@ -7,14 +7,14 @@ export const Card = () => {
     <section className="card">
       <div className="card--container">
         <span className="card--tag">SOULD OUT</span>
-        <img src={cardImg} className="card--img" />
+        <img src={cardImg} className="card--img" alt="Main card image." />
         <div className="card--review">
-          <img src={reviewIcon} className="review--icon" />
+          <img src={reviewIcon} className="review--icon" alt="Star icon." />
           <span className="review--rate">5.0</span>
           <span className="review--count gray">(6) • </span>
           <span className="review--country gray">USA</span>
         </div>
-        <p>Life lessons with Katie Zaferes</p>
+        <h2>Life lessons with Katie Zaferes</h2>
         <p>
           <span className="bold">From $136</span> / person
         </p>
