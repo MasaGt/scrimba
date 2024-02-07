dcd5a91fc62508f6a8c43785fab55b5178719933
MasaGt <gachapin2236tx@gmail.com>
2024-02-07 15:25:22 +1300

    Add more cards

--- aribnb_clone/App.js
+++ aribnb_clone/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavBar } from "./components/NavBar";
 import { Hero } from "./components/Hero";
 import { Card } from "./components/Card";
+import Data from "./data";
 
 /*
 Challenge: Build the Navbar component.
@@ -9,11 +10,24 @@ Check the Figma file for the design specifics.
 */
 
 export default function App() {
+  const cards = Data.map((data, index) => {
+    return (
+      <Card
+        key={index}
+        img={data.coverImg}
+        rating={data.stats.rating}
+        reviewCnt={data.stats.reviewCount}
+        location={data.location}
+        heading={data.title}
+        price={data.price}
+      />
+    );
+  });
   return (
     <div>
       <NavBar />
       <Hero />
-      <Card />
+      <section className="cards">{cards}</section>
     </div>
   );
 }
--- aribnb_clone/components/Card.jsx
+++ aribnb_clone/components/Card.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import cardImg from "Img/katie-zaferes.png";
+// import cardImg from "Img/katie-zaferes.png";
 import reviewIcon from "Img/star.png";
 
-export const Card = () => {
+export const Card = (props) => {
   return (
-    <section className="card">
-      <div className="card--container">
-        <span className="card--tag">SOULD OUT</span>
-        <img src={cardImg} className="card--img" alt="Main card image." />
-        <div className="card--review">
-          <img src={reviewIcon} className="review--icon" alt="Star icon." />
-          <span className="review--rate">5.0</span>
-          <span className="review--count gray">(6) • </span>
-          <span className="review--country gray">USA</span>
-        </div>
-        <h2>Life lessons with Katie Zaferes</h2>
-        <p>
-          <span className="bold">From $136</span> / person
-        </p>
+    <div className="card--container">
+      <span className="card--tag">SOULD OUT</span>
+      <img
+        src={require(`Img/${props.img}`)}
+        className="card--img"
+        alt="Main card image."
+      />
+      <div className="card--review">
+        <img src={reviewIcon} className="review--icon" alt="Star icon." />
+        <span className="review--rate">{props.rating}</span>
+        <span className="review--count gray">({props.reviewCnt}) • </span>
+        <span className="review--country gray">{props.location}</span>
       </div>
-    </section>
+      <h2>{props.heading}</h2>
+      <p>
+        <span className="bold">From ${props.price}</span> / person
+      </p>
+    </div>
   );
 };
